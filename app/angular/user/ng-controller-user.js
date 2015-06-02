6e1068e726165cc53554ac1f7b046737c0a188d8
Younes JAAIDI <yjaaidi@gmail.com>
2015-06-02 09:01:08 +0200

    User edit view.

--- app/angular/user/ng-controller-user.js
+++ app/angular/user/ng-controller-user.js
@@ -9,23 +9,12 @@
 
     angular.module('wishlist').controller('ControllerUser', function ($scope) {
 
-        var userList = [
-            {
-                email: 'foo@bar.com',
-                firstName: 'Foo',
-                lastName: 'BAR'
-            },
-            {
-                email: 'wish@tack.com',
-                firstName: 'Wish',
-                lastName: 'TACK'
-            }
-        ];
+        $scope.user = {};
 
-        $scope.user = userList[0];
+        $scope.saveUser = function saveUser(args) {
 
-        $scope.switchUser = function switchUser(userIndex) {
-            $scope.user = userList[userIndex];
+            $scope.user = angular.copy(args.user);
+
         };
 
     });
