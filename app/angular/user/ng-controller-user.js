baadee3759ac36eb5a85b1bac1cd5a9b588d2231
Younes JAAIDI <yjaaidi@gmail.com>
2015-06-02 21:53:10 +0200

    User api interaction.

--- app/angular/user/ng-controller-user.js
+++ app/angular/user/ng-controller-user.js
@@ -7,16 +7,69 @@
 
 (function () {
 
-    angular.module('wishlist').controller('ControllerUser', function ($scope) {
+    angular.module('wishlist').controller('ControllerUser',
+        function ($http,
+                  $scope) {
 
-        $scope.user = {};
+            var userListUrl = '/api/v1/users/';
 
-        $scope.saveUser = function saveUser(args) {
+            /* Wait for resource before showing anything. */
+            $scope.isViewReady = false;
+            $scope.formUser = {};
+            $scope.user = null;
 
-            $scope.user = angular.copy(args.user);
+            /**
+             * Send user to API.
+             * @param args{user:User}
+             */
+            $scope.saveUser = function saveUser(args) {
 
-        };
+                var method = 'post';
+                var userResourceUrl = userListUrl;
+                var user = args.user;
 
-    });
+                if ($scope.user != null) {
+                    userResourceUrl += $scope.user.id + '/';
+                    method = 'patch';
+                }
+
+                $http[method](userResourceUrl, user).success(function (data) {
+                    $scope.user = data;
+                    $scope.formUser = angular.copy($scope.user);
+                });
+
+            };
+
+            /**
+             * Remove user from API.
+             */
+            $scope.removeUser = function removeUser(args) {
+
+                var user = args.user;
+
+                $http.delete(userListUrl + user.id + '/').success(function () {
+                    $scope.user = null;
+                    $scope.formUser = null;
+                });
+
+            };
+
+            /**
+             * Retrieve users from API.
+             */
+            $http.get(userListUrl).success(function (data) {
+
+                var userList = data.objects;
+
+                if (userList.length > 0) {
+                    $scope.user = userList[0];
+                    $scope.formUser = angular.copy($scope.user);
+                }
+
+                $scope.isViewReady = true;
+
+            });
+
+        });
 
 })();
